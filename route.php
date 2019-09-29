d0a0b7703b338f699be1936093327a0831157d68
Leandro Arsita <leandroalbertoarista@gmail.com>
2019-09-29 18:42:51 -0300

    add the possible cases for the routes and add the default route

--- route.php
+++ route.php
@@ -2,17 +2,56 @@
 require_once "Controllers/EmpresasController.php";
 require_once "Controllers/CohetesController.php";
 require_once "Controllers/UsuariosController.php";
+require_once "script.php"
+
 
 $action = $_GET["action"];
 define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
 
 $cohetesController = new CohetesController();
+$empresasController = new EmpresasController();
+$usuariosController = new UsuariosController();
 
 if($action == '') {
-  $cohetesController->getCohetes();
+  home();
 }else{
-  
+    if (isset($action)){
+        $partesURL = explode("/", $action);
+        switch ($partesURL[0]) {
+            case "cohetes" :
+                $cohetesController->getCohetes();
+                break;
+            case "insertarcohete":
+                $cohetesController->insertarCohete();
+                break;
+            case "borrarcohete":
+                $cohetesController->borrarCohete($partesURL[1]);
+                break;
+            case "usuarios" :
+                $usuariosController->getUsuarios();
+                break;
+            case "insertarusuario":
+                $usuariosController->insertarUsuario();
+                break;
+            case "borrarusuario":
+                $usuariosController->borrarUsuario($partesURL[1]);
+                break;
+            case "empresas":
+                $empresasController->getEmpresas();
+                break;
+            case "insertarempresa":
+                $empresasController->insertarEmpresa();
+                break;
+            case "borrarempresa":
+                $empresasController->borrarEmpresa($partesURL[1]);
+                break;
+            default:
+                home();
+        }
+    }
+    
 }
+
 
 
 ?>
